34852e44d1150f66c1493955a7c02b2ac0abed20
Antoine Bouche <abouche0603@gmail.com>
2024-01-15 13:16:37 +0000

    update(entrypoint): add nominatim env variable

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,9 @@
 cat > ./src/app/environement/environement.ts <<EOF
 export const environment = {
   production: $( [ -n "$API_URL" ] && [ "$ENV" = "PROD" ] && echo "true" || echo "false" ),
-  path: "$( [ -n "$API_URL" ] && [ "$ENV" = "PROD" ] && echo $API_URL )"
+  path: "$( [ -n "$API_URL" ] && [ "$ENV" = "PROD" ] && echo $API_URL )",
+  nominatimAPI: "http://51.77.151.197:8080"
 };
 EOF
 
-ng serve --host 0.0.0.0 --port $PORT
+ng serve --host 0.0.0.0 --port $PORT --disable-host-check
